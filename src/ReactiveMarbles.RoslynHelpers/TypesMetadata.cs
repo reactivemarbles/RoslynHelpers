140fd9aacd6721364d085450f7a5663413fe9a56
Glenn <5834289+glennawatson@users.noreply.github.com>
2022-08-15 13:05:30 +1000

    housekeeping: Make the pragma up the top to stop consumers warnings (#67)

--- src/ReactiveMarbles.RoslynHelpers/TypesMetadata.cs
+++ src/ReactiveMarbles.RoslynHelpers/TypesMetadata.cs
@@ -1,8 +1,8 @@
-﻿// Copyright (c) 2019-2021 ReactiveUI Association Incorporated. All rights reserved.
+﻿#pragma warning disable
+
+// Copyright (c) 2019-2022 ReactiveUI Association Incorporated. All rights reserved.
 // ReactiveUI Association Incorporated licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for full license information.
-
-#pragma warning disable
 
 using System.Collections.Concurrent;
 using System.Collections.Generic;
